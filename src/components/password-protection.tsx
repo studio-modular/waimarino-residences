1f9ae360dab27bc10eee7c6dd8d2195d819b56d7
Jack Jeffress <jack@modular.co>
2025-04-03 22:21:09 +1100

    Added where did you hear about us

--- src/components/password-protection.tsx
+++ src/components/password-protection.tsx
@@ -6,7 +6,19 @@ import Form from "next/form";
 export function PasswordProtection() {
   return (
     <div className="fixed top-0 left-0 w-full h-screen bg-gray-200 z-[100] flex items-center justify-center">
-      <div className="max-w-prose w-full text-center inline-flex gap-4 flex-col">
+      <img
+        alt="Waimarino Lodge"
+        className="object-cover object-center w-full h-full fixed top-0 left-0"
+        data-nimg="1"
+        decoding="async"
+        height="2500"
+        loading="lazy"
+        sizes="(max-width: 768px) 200vw, 100vw"
+        src="https://d1et5tg8povlkp.cloudfront.net/images/indoor-outdoor-3000x1500.jpg?q=100&amp;w=3000"
+        srcSet="https://d1et5tg8povlkp.cloudfront.net/images/indoor-outdoor-1500x750.jpg?q=100&amp;w=750 750w, https://d1et5tg8povlkp.cloudfront.net/images/indoor-outdoor-1500x750.jpg?q=100&amp;w=1500 1500w, https://d1et5tg8povlkp.cloudfront.net/images/indoor-outdoor-2250x1125.jpg?q=100&amp;w=2250 2250w, https://d1et5tg8povlkp.cloudfront.net/images/indoor-outdoor-3000x1500.jpg?q=100&amp;w=3000 3000w"
+        width="5000"
+      ></img>
+      <div className="max-w-prose w-full text-center inline-flex gap-4 flex-col relative z-10 bg-dg-background p-8">
         <Form action={logIn} className="mb-8">
           <Input
             className="text-black text-center border-b-black"
@@ -18,7 +30,6 @@ export function PasswordProtection() {
             Login
           </Button>
         </Form>
-
         <p>
           If you are interested in this project but don&apos;t have a password please contact the project principal:
           Andrew McIntosh
